6ebd2c3330a730f3c81520238ecef422c5faec72
ldNhat <ldnhat.19ce@vku.udn.vn>
2021-07-23 09:33:31 +0700

    lesson6and7

--- DemoMVVM/app/src/main/java/com/ldnhat/demomvvm/TimeUtil.java
+++
@@ -1,8 +0,0 @@
-package com.ldnhat.demomvvm;
-
-public class TimeUtil {
-
-    public static final Long DONE = 0L;
-    public static final Long ONE_SECOND = 1000L;
-    public static final Long COUNTDOWN_TIME = 60000L;
-}
