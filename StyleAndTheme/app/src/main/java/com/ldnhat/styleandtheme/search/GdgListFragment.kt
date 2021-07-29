2238192ef9ff069a61ffcf98e3eec4c58ccce4e7
ldNhat <ldnhat.19ce@vku.udn.vn>
2021-07-29 16:01:45 +0700

    update demo chip

--- StyleAndTheme/app/src/main/java/com/ldnhat/styleandtheme/search/GdgListFragment.kt
+++ StyleAndTheme/app/src/main/java/com/ldnhat/styleandtheme/search/GdgListFragment.kt
@@ -12,8 +12,10 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.location.*
+import com.google.android.material.chip.Chip
 import com.google.android.material.snackbar.Snackbar
 import com.ldnhat.styleandtheme.databinding.FragmentGdgListBinding
+import com.ldnhat.styleandtheme.R
 
 private const val LOCATION_PERMISSION_REQUEST = 1
 
@@ -54,6 +56,31 @@ class GdgListFragment : Fragment() {
                     ).show()
                 }
             })
+
+        viewModel.regionList.observe(viewLifecycleOwner, object : Observer<List<String>>{
+            override fun onChanged(data: List<String>?) {
+                data ?: return
+
+                val chipGroup = binding.regionList
+                val inflator = LayoutInflater.from(chipGroup.context)
+
+                val children = data.map { regionName ->
+                    val chip = inflator.inflate(R.layout.region, chipGroup, false) as Chip
+                    chip.text = regionName
+                    chip.tag = regionName
+                    
+                    chip.setOnCheckedChangeListener { button, isChecked ->
+                        viewModel.onFilterChanged(button.tag as String, isChecked)
+                    }
+                    chip
+                }
+                chipGroup.removeAllViews()
+
+                for (chip in children) {
+                    chipGroup.addView(chip)
+                }
+            }
+        })
 
         setHasOptionsMenu(true)
         return binding.root
