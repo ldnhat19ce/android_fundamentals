1c312a4e2bad72eb01413f0ba4524a6c93c9d70e
ldNhat <ldnhat.19ce@vku.udn.vn>
2021-07-29 14:51:47 +0700

    update

--- StyleAndTheme/app/src/main/java/com/ldnhat/styleandtheme/home/HomeFragment.kt
+++ StyleAndTheme/app/src/main/java/com/ldnhat/styleandtheme/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.ldnhat.styleandtheme.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
@@ -23,6 +24,14 @@ class HomeFragment : Fragment() {
         val binding = HomeFragmentBinding.inflate(inflater)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
+        binding.homeViewModel = viewModel
+
+        viewModel.navigateToSearch.observe(viewLifecycleOwner, {
+            if (it){
+                this.findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToGdgListFragment())
+                viewModel.onNavigatedToSearch()
+            }
+        })
         return binding.root
     }
 }
